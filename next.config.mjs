f1bb30fccbdb5ad42d0ad21287b0ad9d40575bfe
Sayri <sayridev@gmail.com>
2025-08-22 02:32:39 -0500

    final

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  // Configuración para Next.js que genera un sitio estático
+  output: 'export',
+  // La base de la URL para que las rutas funcionen correctamente en GitHub Pages
+  basePath: '/hoteles-rosas', // Asegúrate de que este sea el nombre de tu repositorio
+  trailingSlash: true, // Opcional, pero recomendado para algunas rutas
+
+  // Configuraciones que ya tenías
   eslint: {
     ignoreDuringBuilds: true,
   },
